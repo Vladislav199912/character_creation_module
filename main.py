e01a2ba95fd805346e790c1d96d9875d515cdacf
Владислав Ермаков <vladermakov@mail.ru>
2023-03-14 22:16:34 +0500

    With comments

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from inspect import cleandoc
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """В зависимости от класса выбираем урон, который он наносит."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 +randint(3, 5)}')
     if char_class == 'mage':
@@ -11,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """В зависимости от класса выбираем количество блокированного урона."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -21,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """В зависимости от класса выбираем количество очков умений."""
     if char_class == 'warrior':
         return (f'{char_name}применил специальное умение«Выносливость{80+25}»')
     if char_class == 'mage':
@@ -31,6 +37,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Выбор класса и тренеровка."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -56,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор класса и описание класса."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -79,7 +87,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -91,4 +100,8 @@ def main() -> str:
     print(start_training(char_name, char_class))
 
 
-main()
+print(cleandoc(attack.__doc__))
+print(cleandoc(defence.__doc__))
+print(cleandoc(special.__doc__))
+print(cleandoc(start_training.__doc__))
+print(cleandoc(choice_char_class.__doc__))
